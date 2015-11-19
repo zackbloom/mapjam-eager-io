d6d571df085cdf9d33b9074f1576008d3ed4e8e4
Bob Axford <bob@figjamit.com.au>
2015-11-19 16:51:07 +1100

    advanced options, rework preview function. aspect ratio

--- app.js
+++ app.js
@@ -4,50 +4,31 @@
 
   // We check for features which are not universally supported, and don't try to
   // show the app if it would error.
-  if (!window.addEventListener)
-    return;
+  if (!window.addEventListener) return;
 
-  // The INSTALL_OPTIONS constant is inserted by the Eager bundler.  It's value is the
-  // value of the options defined in the install.json file.
-  var options = INSTALL_OPTIONS;
+  var options, ready, el;
 
-  // We fake a view count by storing it locally.  It only updates for the currently
-  // viewing visitor, making it pretty much useless.
+  options = INSTALL_OPTIONS;
 
-  var el = null;
-  var prevEl = null;
-
-  var updateElement = function(){
-    console.log('updateElement');
-    // We keep track of the last element to allow us to restore the removed element
-    // when we do live updating of the preview.  Details:
-    // https://eager.io/developer/docs/install-json/preview#dealing-with-element-fields
-    if (el && el.parentNode){
-      if (prevEl){
-        el.parentNode.replaceChild(prevEl, el);
-        prevEl = null;
-      } else {
-        el.parentNode.removeChild(el);
-      }
+  ready = function(fn) {
+    console.log("READY");
+    if (document.readyState != 'loading'){
+      fn();
     } else {
-      if (options.element.method == 'replace')
-        prevEl = document.querySelector(options.element.selector);
-
-      el = Eager.createElement(options.element);
+      document.addEventListener('DOMContentLoaded', fn);
     }
   };
 
-  var update = function(){
-    console.log(options);
-    updateElement();
-    // build the querystring for embeds based on user input
+  ready(function(){
+    console.log("DFUNC");
+    el = Eager.createElement(options.element);
     var params = {
-        app_url: '//mapjam.com/',
-        cdn_url: '//mapjamjson.global.ssl.fastly.net/',
-        map_id: options.map,
-        container: 'mapjam-1',
-        domain: 'mapjam.com',
-        disableClusteringAtZoom: 1
+      app_url: '//mapjam.com/',
+      cdn_url: '//mapjamjson.global.ssl.fastly.net/',
+      map_id: options.map,
+      container: 'mapjam-1',
+      domain: 'mapjam.com',
+      disableClusteringAtZoom: 1
     };
     if (options.zoom) {
       params.zoom = Math.max(Math.min(options.zoom, 18), 1);
@@ -55,10 +36,10 @@
     if (options.sharing_visible) {
       params.sharing_visible = !options.sharing_visible;
     }
-    function validLat(lat) {
-      return lat !== null && lat !== undefined && !isNaN(lat);
+    function validNum(val) {
+      return val !== null && val !== undefined && !isNaN(val);
     }
-    if (validLat(options.lat) && validLat(options.lng)) {
+    if (validNum(options.lat) && validNum(options.lng)) {
       params.map_lat_lng = options.lng + ',' + options.lat;
     }
     var parts = [];
@@ -69,26 +50,24 @@
     }
     var qs = parts.join("&");
     console.log('qs: ' + qs);
-    el.innerHTML = '<iframe class="mapjam-iframe" frameborder="0" id="mapjam-iframe" src="//embeds.mapjam.com/v2/map-embed.html?' + qs + '" style="width: 100%;height:{{height_px}}px"></iframe>';
-  };
-
-  var setOptions = function(opts){
-    options = opts;
-
-    update();
-  };
-
-  // Since we're adding an element to the body, we need to wait until the DOM is
-  // ready before inserting our widget.
-  if (document.readyState == 'loading')
-    document.addEventListener('DOMContentLoaded', update);
-  else
-    update();
-
-  // This is used by the preview to enable live updating of the app while previewing.
-  // See the preview.handlers section of the install.json file to see where it's used.
-  window.MapjamWidget = {
-    setOptions: setOptions
-  };
+    var style =  '<style>' +
+        '.mapjam-iframe {' +
+        'position: relative;' +
+        'width: 100%;' +
+        'height: 0;' +
+        'padding-bottom: 80%;' +
+        '}' +
+        '.eager-google-map {' +
+        'position: absolute;' +
+        'top: 0;' +
+        'height: 100%;' +
+        'left: 0;' +
+        'width: 100%;' +
+        'background: #e5e3df;' +
+        '}' +
+        '</style>';
+    var ifr = '<div><h1>TEST</h1><iframe class="mapjam-iframe" frameborder="0" id="mapjam-iframe" src="//embeds.mapjam.com/v2/map-embed.html?' + qs + '" style="width: 100%;height:100px;padding-bottom:' + 80 + '%"></iframe></div>';
+    el.innerHTML = ifr;
+  });
 
 })();
