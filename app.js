44ab59c4409f842cbe210df4eb002473796f6d6b
Zack Bloom <zackbloom@gmail.com>
2015-11-19 14:03:22 -0500

    Improvements to field descriptions and flip lat/lng

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function(){
 
-  var mapRegex = /mapjam.com\/([\w-]*)/i
+  var mapRegex = /mapjam.com\/([\w\-]*)/i
 
   // We check for features which are not universally supported, and don't try to
   // show the app if it would error.
@@ -18,44 +18,42 @@
     }
   };
 
-  var map = options.map;
-  if(!map) {
-    console.log('No map specified, exiting');
-    return;
-  }
+  var map = options.map || 'world';
 
   var matches = map.match(mapRegex);
   if (matches) {
     map = matches[1];
   }
+
   ready(function(){
     el = Eager.createElement(options.element);
+
     var params = {
       app_url: '//mapjam.com/',
       cdn_url: '//mapjamjson.global.ssl.fastly.net/',
       map_id: map,
       container: 'mapjam-1',
       domain: 'mapjam.com',
-      disableClusteringAtZoom: 1
+      disableClusteringAtZoom: 1,
+      sharing_visible: options.sharingVisible
     };
-    if (options.advanced.zoom !== '-1') {
-      params.zoom = options.advanced.zoom;
+
+    if (options.showCustomZoom && options.zoom) {
+      params.zoom = options.zoom;
     }
-    params.sharing_visible = options.advanced.sharing_visible;
-    function validNum(val) {
-      return val !== null && val !== undefined && !isNaN(val);
+
+    if (options.showCustomLocation) {
+      params.map_lat_lng = options.location.lat + ',' + options.location.lng;
     }
-    if (validNum(options.advanced.lat) && validNum(options.advanced.lng)) {
-      params.map_lat_lng = options.advanced.lng + ',' + options.advanced.lat;
-    }
+
     var parts = [];
     for (var i in params) {
       if (params.hasOwnProperty(i)) {
         parts.push(encodeURIComponent(i) + "=" + encodeURIComponent(params[i]));
       }
     }
+
     var qs = parts.join("&");
-    //console.log('qs: ' + qs);
     var style =  '<style>' +
         '.mapjam-holder {' +
         'position: relative;' +
@@ -70,9 +68,11 @@
         'width: 100%;' +
         '}' +
         '</style>';
+
     var ifr = '<iframe class="mapjam-iframe" frameborder="0" id="mapjam-iframe" src="//embeds.mapjam.com/v2/map-embed.html?' + qs + '" style="width:100%;height:100%;"></iframe>';
-    var thing = '<div class="mapjam-holder">' + style + ifr + '</div>';
-    el.innerHTML = thing;
+    var holder = '<div class="mapjam-holder">' + style + ifr + '</div>';
+
+    el.innerHTML = holder;
   });
 
 })();
